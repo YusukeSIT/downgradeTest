15d03f589197b72e9b486209974237cc7c82ead0
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2024-09-25 05:54:25 +0900

    Update examples.js

--- examples.js
+++ examples.js
@@ -16,7 +16,7 @@ AFRAME.registerComponent('trigger', {
         -5 - Math.random() * 2
       ),
       rotation: new THREE.Euler(Math.random() * 1 - .5, 0, 0),
-      color: new THREE.Color( 0000ff00 )
+      color: new THREE.Color( 0x0000ff )
     });
   }
 });
